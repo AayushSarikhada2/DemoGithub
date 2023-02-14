85733f873ba12d6c0a45e51a86226257beb60019
AayushSarikhada2 <learnlikem@gmail.com>
2023-02-14 10:20:19 +0530

    added some code

--- Demo_Explore_XCode/ViewController.swift
+++ Demo_Explore_XCode/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var NumLabel: UILabel!
+    
+    static var click: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ViewController.click = Int(NumLabel.text ?? "0") ?? 1
+        
     }
 
-
+    @IBAction func Fetch(_ sender: UIButton) {
+        ViewController.click = ViewController.click+1
+        NumLabel.text = String(ViewController.click)
+        
+    }
+    
 }
 
